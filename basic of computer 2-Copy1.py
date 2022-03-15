e54136f1a5ac485a57719433da2f7181d5d4bfba
Abhimishra2812 <101655063+Abhimishra2812@users.noreply.github.com>
2022-03-15 22:49:58 +0530

    Update basic of computer 2-Copy1.py

--- basic of computer 2-Copy1.py
+++ basic of computer 2-Copy1.py
@@ -4,13 +4,13 @@
 # In[ ]:
 
 
-get_ipython().set_next_input('1 )What is diffrence between RAM and ROM');get_ipython().run_line_magic('pinfo', 'ROM')
+1 )What is diffrence between RAM and ROM');get_ipython().run_line_magic('pinfo', 'ROM')
 Ans:1)RAM is a form of computer memory that can be read and changed in any order,
     typically used to store working data and machine code.
     
     2)ROM is a type of non-volatile memory used in computers and other electronic devices.
     
-get_ipython().set_next_input('2 )What is memory?What is primary and secondary memory');get_ipython().run_line_magic('pinfo', 'memory')
+2 )What is memory?What is primary and secondary memory');get_ipython().run_line_magic('pinfo', 'memory')
 Ans:Memory unit is the amount of data that can be stored in the storage unit.
     This storage capacity is expressed in terms of Bytes.There are 2 types of memory primary and secondary
     memory 
@@ -28,30 +28,30 @@ Ans:Memory unit is the amount of data that can be stored in the storage unit.
     g.EPROM:ERASABLE PROGRAMMABLE READ ONLY MEMORY
     h.EEPROM:ELETRICALLY ERASABLE PROGRAMMABLE READ ONLY MEMORY
         
-get_ipython().set_next_input('4 )What is a keyboard?What are its function keys and what purpose do they serve');get_ipython().run_line_magic('pinfo', 'serve')
+4 )What is a keyboard?What are its function keys and what purpose do they serve');get_ipython().run_line_magic('pinfo', 'serve')
 Ans:Function Keys are keys on keyboard which cause operating system to command interpreter or
     an application to perform certain actions on the screen.There are 12 keys on the keyboard
     from F1 to F12.All keys are having different functions to perform.
     
-get_ipython().set_next_input('5 )What is a printer? How do you connect it? What is the shortcut key to print');get_ipython().run_line_magic('pinfo', 'print')
+5 )What is a printer? How do you connect it? What is the shortcut key to print');get_ipython().run_line_magic('pinfo', 'print')
 Ans:A printer is a hardware output device that is used to generate hard copy and print any document.
     To Connect a printer here are the following steps
     .In Excel, click File > Print. Tip: You can also use the keyboard shortcut, Ctrl + P.
     .Click the Printer drop-down arrow, and select the printer you want to connect to.
          The shortcut key to print is {Ctrl-P}
         
-get_ipython().set_next_input('6 )What are the types of printer');get_ipython().run_line_magic('pinfo', 'printer')
+6 )What are the types of printer');get_ipython().run_line_magic('pinfo', 'printer')
 Ans:There are 2 types of printer :
     .impact
     .non impact
     
-get_ipython().set_next_input('7 )What is a light pen?Describe its functions');get_ipython().run_line_magic('pinfo', 'functions')
+7 )What is a light pen?Describe its functions');get_ipython().run_line_magic('pinfo', 'functions')
 Ans:A light pen is a computer input device in the form of a light-sensitive wand use
     in conjunction with a computer's  cathode-ray tube (CRT) display. It allows the user
     to point to displayed objects or draw on the screen in a similar wayto a touchscreen
     but with greater positional accuracy.
     
-get_ipython().set_next_input('8 )Describe the process to scan a document and store it in your computer');get_ipython().run_line_magic('pinfo', 'computer')
+8 )Describe the process to scan a document and store it in your computer');get_ipython().run_line_magic('pinfo', 'computer')
 Ans:To scan a document and to store it here are the following steps:
        
      .Load your document.
